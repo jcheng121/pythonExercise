4b8258a2c3e213fc80809c8eeea221e491cf08e6
Jiwei Cheng <jiwei.cheng@harman.com>
2016-07-25 14:33:23 -0400

    modify function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,6 @@
 
 def calculateSalary (timeinHour, rate):
+    "Function to calculate salary, note if you work more than 40 hours a week, your rate is bumped"
     if timeinHour > 40 : rate = rate * 1.5
     return timeinHour * rate
 
@@ -13,22 +14,28 @@ def inputNameSalry ():
     print "Your rate is ", yourRate
 
 def inputScore():
-    canIQuit = False
-    while (not canIQuit):
-        try:
-            what = raw_input("Enter score between 0 and 1.0 : (q to quit)")
-            if what == "q" :
-                print "quit from score input rootine!!!"
-                canIQuit = True;
-            else:
-                score = float(what)
-                if score > 1 or score < 0 : print "Bad score "
-                elif score >= 0.9 : print "A"
-                elif score >= 0.8 : print "B"
-                elif score >= 0.7 : print "C"
-                elif score >= 0.6 : print "D"
-                else              : print "F"
-        except:
-            print "Please enter a number between 0, and 1.0"
+    strScore = None
+    try:
+        what = raw_input("Enter score between 0 and 1.0 : (q to quit)")
+        if what == "q" :
+            strScore = what;
+        else:
+           score = float(what)
+           if score > 1 or score < 0 : print "Bad score "
+           elif score >= 0.9 : strScore ="A"
+           elif score >= 0.8 : strScore ="B"
+           elif score >= 0.7 : strScore ="C"
+           elif score >= 0.6 : strScore ="D"
+           else              : strScore ="F"
+    except:
+        print "Please enter a number between 0, and 1.0"
+    return strScore;
 
-max(40, 20)
+returnScore = None
+while ( not(returnScore == "q")):
+    returnScore = inputScore()
+    print returnScore
+
+print "Done!"
+
+
