fddf7d071da9336b0a9bf3bc046cdf88f345bbcb
Jiwei Cheng <chengjiwei@gmail.com>
2016-07-28 15:08:35 -0400

    add handling to know method call vs signal

--- parseDbus.py
+++ parseDbus.py
@@ -1,6 +1,27 @@
 #! /usr/bin/python
 import json
 
+def analyzeFrom (fmLine) :
+    fromDict = {}
+    lookfor = {"method call sender=:":["method call","dest"],
+               "signal sender=:":["signal","dest"]}
+    for lookforSender in lookfor :
+        pos = fmLine.find(lookforSender)
+        if pos >= 0 : 
+            fmLine = fmLine[len(lookforSender):]
+            content = lookfor[lookforSender]
+            if not content[1] is None :
+                findDestPos = fmLine.find(" -> " + content[1])
+                fromSrc = fmLine[:findDestPos]
+                fromSrc = fromSrc.strip()
+                dest = fmLine[findDestPos+len(" -> " + content[1])+1:]
+                findDestPos = dest.find("serial=")
+                dest = dest[:findDestPos]
+                fromDict[content[0]] = fromSrc
+                fromDict[content[1]] = dest  
+                fromDict["message"]  = content[0]
+                return fromDict
+        
 def printList (list):
     for key in list:
         if type(key).__name__ == 'dict':
@@ -31,44 +52,53 @@ matchingArray = ["selectDevice",
                  "operStatus",
                  "audioSources",
                  "start",
-                 "deviceState"]
+                 "pause",
+                 "stop",
+                 "deviceState",
+                 "playState",
+                 "nowPlaying"]
 messageStatistic = {}
 
 for matching in matchingArray :
     messageStatistic[matching] = 0
-
-dbusHandle = open ('dbus.log', 'r')
-
-for line in dbusHandle :
-    line = line.strip()
-    if lookingFor is None :
-        for matching in matchingArray:
-            lookingFor = matching
-            matching = "string \"" + matching + "\""
-            pos = line.find(matching)
-            if pos < 0:
-                lookingFor = None
+    
+originalFile= []
+commandForm = {}
+with open ('dbus.log', 'r') as dbusHandle :    
+    for line in dbusHandle :
+        line = line.strip()  
+        originalFile.append(line) 
+        if lookingFor is None :
+            for matching in matchingArray:
+                lookingFor = matching
+                matching = "string \"" + matching + "\""
+                pos = line.find(matching)
+                if pos < 0:
+                    lookingFor = None
+                    continue
+                else:
+                    commandForm = analyzeFrom(originalFile[len(originalFile)-2])
+                    break
+        else:
+            line = line.replace("string \"","")
+            if not keepLine is None :
+                line = keepLine + line
+                line = line.replace(r'\n',"")
+                line = line.rstrip("\"")
+                keepLine = None
+            try:
+                line = line.rstrip("\"")
+                audioDict = json.loads(line)
+            except:
+                keepLine = line
                 continue
-            else:
-                break
-    else:
-        line = line.replace("string \"","")
-        if not keepLine is None :
-            line = keepLine + line
-            line = line.replace(r'\n',"")
-            line = line.rstrip("\"")
+            index = messageStatistic[lookingFor] = messageStatistic[lookingFor] + 1
+            print "="*80
+            print "Found %d %s %s !!" % (index, lookingFor, commandForm["message"])    
+            print "-"*80
+            printDict(commandForm, True)    
+            print "-"*80    
+            printDict(audioDict, False)
             keepLine = None
-        try:
-            line = line.rstrip("\"")
-            audioDict = json.loads(line)
-        except:
-            keepLine = line
-            continue
-        index = messageStatistic[lookingFor] = messageStatistic[lookingFor] + 1
-        print "=========================================================================================="
-        print "Found %d %s message!!!" % (index, lookingFor)
-        print "=========================================================================================="
-        printDict(audioDict, False)
-        keepLine = None
-        lookingFor = None
+            lookingFor = None
 dbusHandle.close()
